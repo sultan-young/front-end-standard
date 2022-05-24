eea2c098394889813b6ce22a1e63786ef311ae42
yangzilong <yangzilong@hungrypandagroup.com>
2022-05-24 21:08:08 +0800

    feat: 增加prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,21 @@ module.exports = {
     "plugins": [
         "@typescript-eslint"
     ],
-    "rules": {
-        "no-console": 2
+    "rules":{
+        // 禁止对象字面量中出现重复的 key
+        "no-dupe-keys": "error",
+        // 禁止出现重复的 case 标签
+        "no-duplicate-case": "error",
+        // 禁止出现空语句块,允许catch出现空语句
+        "no-empty": ["error", {"allowEmptyCatch": true}],
+        // 禁止对 catch 子句的参数重新赋值
+        "no-ex-assign":"error",
+        // 禁止不必要的布尔转换
+        "no-extra-boolean-cast": "error",
+        // 禁止不必要的分号
+        "no-extra-semi": "error",
+        // 强制所有控制语句使用一致的括号风格
+        "curly": "error"
+        
     }
 }
---
+++ src/test.js
@@ -0,0 +1,4 @@
+console.log('test');
+
+let a = 5;
+const arr = [1, 3, 5];
